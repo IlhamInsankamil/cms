cb7eb9f44700aae70e8a4d922a957baa71f41958
unknown <ilham.insan.kamil@gmail.com>
2015-03-14 11:30:09 +0700

    2nd improve

--- includes/cmsApplication.php
+++ includes/cmsApplication.php
@@ -3,4 +3,15 @@ require_once('cmsBase.php');
 class CmsApplication extends CmsBase{
 	// semua kode yang ada disini dapat
 	// diakses melalui fungsi utama dalam CMS.
+
+	function addcontent(){
+		echo "Here add content functionally will takes place";
+	}
+	function viewcontent(){
+		echo "Here view content functionally will takes place";
+	}
+	function anyothertask(){
+		echo "This is another task, here can be written a complete PHP program against that task";
+	}
+
 }
